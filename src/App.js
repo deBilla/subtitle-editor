bef2b5d3ca9ae1722dd6fd45d511a27c6770dd26
debilla <dimuthu.billa@gmail.com>
2023-06-22 17:51:36 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import WebVTT from 'node-webvtt';
 
 function App() {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleFileUpload = () => {
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const subtitleData = reader.result;
+        const parsedSubtitle = WebVTT.parse(subtitleData);
+        let cueArr = parsedSubtitle.cues.slice(11);
+
+        for (let i =0; i<cueArr.length; i++) {
+            cueArr[i]['identifier'] = (i+1).toString();
+        }
+
+        parsedSubtitle.cues = cueArr;
+
+        const modifiedSubtitleContent = WebVTT.compile(parsedSubtitle);
+        const modifiedSubtitleBlob = new Blob([modifiedSubtitleContent], { type: 'text/vtt' });
+        const downloadLink = URL.createObjectURL(modifiedSubtitleBlob);
+        const a = document.createElement('a');
+        a.href = downloadLink;
+
+        const originalFilename = file.name;
+
+        a.download = `modified_${originalFilename}`;
+        a.click();
+      };
+      reader.readAsText(file);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Subtitle File Processor</h1>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleFileUpload}>Process and Download</button>
     </div>
   );
 }
