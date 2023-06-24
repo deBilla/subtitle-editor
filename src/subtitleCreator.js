5f7091678530bfec4304c0601f35419260675462
debilla <dimuthu.billa@gmail.com>
2023-06-24 13:57:34 +0800

    date and text area

--- src/subtitleCreator.js
+++ src/subtitleCreator.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import WebVTT from "node-webvtt";
+import DateTime from "react-datetime";
+import "react-datetime/css/react-datetime.css";
+import moment from "moment";
 
 function SubtitleCreator() {
   const [subtitles, setSubtitles] = useState([]);
@@ -7,34 +10,45 @@ function SubtitleCreator() {
   const [endTime, setEndTime] = useState("");
   const [subtitleText, setSubtitleText] = useState("");
 
-  const handleStartTimeChange = (event) => {
-    setStartTime(event.target.value);
-  };
-
-  const handleEndTimeChange = (event) => {
-    setEndTime(event.target.value);
-  };
-
   const handleSubtitleTextChange = (event) => {
     setSubtitleText(event.target.value);
   };
 
+  const convertToSeconds = (timeString) => {
+    const timeParts = timeString.split(":");
+    const hours = parseInt(timeParts[0], 10);
+    const minutes = parseInt(timeParts[1], 10);
+    const secs = parseInt(timeParts[2], 10);
+
+    return hours * 3600 + minutes * 60 + secs;
+  };
+
+  const convertSecondsToTime = (totalSeconds) => {
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+
+    const formattedTime = `${String(hours).padStart(2, "0")}:${String(
+      minutes
+    ).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+    return formattedTime;
+  };
+
   const handleAddSubtitle = () => {
     const newSubtitle = {
-      startTime,
-      endTime,
+      startTime: convertToSeconds(startTime),
+      endTime: convertToSeconds(endTime),
       text: subtitleText,
     };
     setSubtitles([...subtitles, newSubtitle]);
-    setStartTime("");
-    setEndTime("");
+    setStartTime(endTime);
     setSubtitleText("");
   };
 
   const handleGenerateSubtitleFile = () => {
     const parsedSubtitle = {
       cues: [],
-      valid: true
+      valid: true,
     };
 
     subtitles.forEach((subtitle, index) => {
@@ -43,7 +57,7 @@ function SubtitleCreator() {
         start: subtitle.startTime,
         end: subtitle.endTime,
         text: subtitle.text,
-        styles: ''
+        styles: "",
       };
       parsedSubtitle.cues.push(cue);
     });
@@ -59,24 +73,42 @@ function SubtitleCreator() {
     a.click();
   };
 
+  const handleStartTimeChange = (selectedTime) => {
+    const formattedTime = moment(selectedTime).format("HH:mm:ss");
+    setStartTime(formattedTime);
+  };
+
+  const handleEndTimeChange = (selectedTime) => {
+    const formattedTime = moment(selectedTime).format("HH:mm:ss");
+    setEndTime(formattedTime);
+  };
+
   return (
     <div>
       <h1>Subtitle Creator</h1>
-      <div>
-        <input
-          type="text"
-          placeholder="Start time"
+      <div style={{ display: "flex" }}>
+        <label style={{ marginRight: "5px" }}>Start Time: </label>
+        <DateTime
           value={startTime}
           onChange={handleStartTimeChange}
+          dateFormat={false}
+          timeFormat="HH:mm:ss"
         />
-        <input
-          type="text"
-          placeholder="End time"
+        <label style={{ marginRight: "5px", marginLeft: "5px" }}>
+          End Time:{" "}
+        </label>
+        <DateTime
           value={endTime}
           onChange={handleEndTimeChange}
+          dateFormat={false}
+          timeFormat="HH:mm:ss"
         />
-        <input
-          type="text"
+        <label style={{ marginRight: "5px", marginLeft: "5px" }}>
+          Subtitles:{" "}
+        </label>
+        <textarea
+          rows={4}
+          cols={30}
           placeholder="Subtitle text"
           value={subtitleText}
           onChange={handleSubtitleTextChange}
@@ -87,7 +119,11 @@ function SubtitleCreator() {
         <h2>Subtitles:</h2>
         {subtitles.map((subtitle, index) => (
           <div key={index}>
-            <p>{`[${subtitle.startTime} - ${subtitle.endTime}]: ${subtitle.text}`}</p>
+            <p>{`[${convertSecondsToTime(
+              subtitle.startTime
+            )} - ${convertSecondsToTime(subtitle.endTime)}]: ${
+              subtitle.text
+            }`}</p>
           </div>
         ))}
       </div>
