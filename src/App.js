27aa613b975476f24fcd6d00daf99760d6d187c6
debilla <dimuthu.billa@gmail.com>
2023-10-12 11:35:52 +0800

    5

--- src/App.js
+++ src/App.js
@@ -26,41 +26,40 @@ function App() {
     setShowSubtitleCreator(!showSubtitleCreator);
   };
 
-  /** Khairis change */
-  // const handleFileUpload = () => {
-  //   if (file) {
-  //     const reader = new FileReader();
-  //     reader.onload = () => {
-  //       const subtitleData = reader.result;
-  //       const parsedSubtitle = WebVTT.parse(subtitleData);
+  const handleFileUploadForRemovingFirst11Subs = () => {
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const subtitleData = reader.result;
+        const parsedSubtitle = WebVTT.parse(subtitleData);
 
-  //       // removing first 11 subs
-  //       let cueArr = parsedSubtitle.cues.slice(11);
+        // removing first 11 subs
+        let cueArr = parsedSubtitle.cues.slice(11);
 
-  //       for (let i = 0; i < cueArr.length; i++) {
-  //         cueArr[i]["identifier"] = (i + 1).toString();
-  //       }
+        for (let i = 0; i < cueArr.length; i++) {
+          cueArr[i]["identifier"] = (i + 1).toString();
+        }
 
-  //       parsedSubtitle.cues = cueArr;
+        parsedSubtitle.cues = cueArr;
 
-  //       const modifiedSubtitleContent = WebVTT.compile(parsedSubtitle);
-  //       const modifiedSubtitleBlob = new Blob([modifiedSubtitleContent], {
-  //         type: "text/vtt",
-  //       });
-  //       const downloadLink = URL.createObjectURL(modifiedSubtitleBlob);
-  //       const a = document.createElement("a");
-  //       a.href = downloadLink;
+        const modifiedSubtitleContent = WebVTT.compile(parsedSubtitle);
+        const modifiedSubtitleBlob = new Blob([modifiedSubtitleContent], {
+          type: "text/vtt",
+        });
+        const downloadLink = URL.createObjectURL(modifiedSubtitleBlob);
+        const a = document.createElement("a");
+        a.href = downloadLink;
 
-  //       const originalFilename = file.name;
+        const originalFilename = file.name;
 
-  //       a.download = `modified_${originalFilename}`;
-  //       a.click();
+        a.download = `modified_${originalFilename}`;
+        a.click();
 
-  //       setSubtitles(cueArr);
-  //     };
-  //     reader.readAsText(file);
-  //   }
-  // };
+        setSubtitles(cueArr);
+      };
+      reader.readAsText(file);
+    }
+  };
 
   const handleFileUpload = () => {
     if (file) {
